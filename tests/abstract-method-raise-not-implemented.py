cf39e3faf6add4c4bc3152b351b44d78062095fd
DOCtorActoAntohich <fairy666death@mail.ru>
2023-09-26 14:16:29 +0300

    add checks for abstract methods

---
+++ tests/abstract-method-raise-not-implemented.py
@@ -0,0 +1,28 @@
+from abc import ABC, abstractmethod
+from typing import List
+
+
+class Service(ABC):
+    @abstractmethod
+    def good(self, a: int, b: float) -> None:
+        raise NotImplementedError
+
+    @abstractmethod
+    def still_okay(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def bad(self, input_size: int) -> float:
+        # ruleid: abstract-method-raise-not-implemented
+        ...
+
+    @abstractmethod
+    def bad_but_with_pass(self):
+        # ruleid: abstract-method-raise-not-implemented
+        pass
+
+    @abstractmethod
+    def bad_because_of_implementation(self, something) -> List:
+        # ruleid: abstract-method-raise-not-implemented
+        result = [i * 2 for i in range(10)]
+        return result
